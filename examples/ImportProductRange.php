769089e2c9b61002352d8f5d8c5fc0e7c7f1e56f
Casper Lai <casper.lai@sleepingdesign.com>
2018-07-29 11:27:42 +0800

    Add product range import model

---
+++ examples/ImportProductRange.php
@@ -0,0 +1,46 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+use GorillaDash\DataMigrationTool\Client;
+use GorillaDash\DataMigrationTool\Models\ProductRange;
+use GuzzleHttp\Exception\GuzzleException;
+
+$client = new Client('__PUT_YOUR_ORGANISATION_KEY__', '__PUT_YOUR_ORGANISATION_SECRET__');
+
+$media = new ProductRange([
+    'batch_id' => 'TEST-BATCH-ID',
+    'name' => 'string',
+    'slug' => 'string',
+    'status' => 'Active', // Active, Paused
+    'menu_label' => 'string',
+    'heading' => 'string',
+    // ==== above required =====
+    // thumbnail, banner, square, rectangle, Gallery
+    'media' => [
+        'banner' => [
+            // 1 media in 1 array.
+            'https://www.sample-videos.com/img/Sample-jpg-image-100kb.jpg'
+        ],
+        'gallery' => [
+            'https://www.sample-videos.com/img/Sample-jpg-image-100kb.jpg',
+            'https://www.sample-videos.com/img/Sample-jpg-image-200kb.jpg',
+        ],
+    ],
+    'sub_heading' => 'string',
+    'caption' => 'string',
+    'description' => 'string',
+    'page_heading' => 'string',
+    'page_subheading' => 'string',
+    'update' => false,
+    'product_categories' => [
+        'product_category_slug_1',
+        'product_category_slug_2',
+        'product_category_slug_3',
+    ],
+]);
+
+try {
+    $response = $client->request($media);
+} catch (GuzzleException $ex) {
+    die($ex->getMessage());
+}
---
+++ src/Models/ProductRange.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace GorillaDash\DataMigrationTool\Models;
+
+/**
+ * Class ProductCategory
+ *
+ * @package GorillaDash\DataMigrationTool\Models
+ */
+class ProductRange extends Model
+{
+
+    /**
+     * Set GraphQL mutation field name
+     *
+     * @var string
+     */
+    protected $field = 'productRange';
+
+    /**
+     * @var
+     */
+    protected $name;
+    /**
+     * @var
+     */
+    protected $menuLabel;
+    /**
+     * @var
+     */
+    protected $status;
+    /**
+     * @var
+     */
+    protected $slug;
+    /**
+     * @var
+     */
+    protected $heading;
+    /**
+     * @var
+     */
+    protected $subHeading;
+    /**
+     * @var
+     */
+    protected $caption;
+    /**
+     * @var
+     */
+    protected $description;
+    /**
+     * @var
+     */
+    protected $meta;
+    /**
+     * @var
+     */
+    protected $pageHeading;
+    /**
+     * @var
+     */
+    protected $pageSubheading;
+    /**
+     * @var
+     */
+    protected $media;
+    /**
+     * @var
+     */
+    protected $update;
+
+    /**
+     * @var
+     */
+    protected $productCategories;
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return mixed
+     */
+    public function toArray()
+    {
+        return [
+            'batch_id' => $this->batchId,
+            'name' => $this->name,
+            'menu_label' => $this->menuLabel,
+            'status' => $this->status,
+            'slug' => $this->slug,
+            'heading' => $this->heading,
+            'sub_heading' => $this->subHeading,
+            'caption' => $this->caption,
+            'description' => $this->description,
+            'meta' => $this->meta,
+            'page_heading' => $this->pageHeading,
+            'page_subheading' => $this->pageSubheading,
+            'media' => base64_encode(json_encode($this->media)),
+            'update' => $this->update,
+            'product_categories' => $this->productCategories,
+        ];
+    }
+}
